3d11e502676bda4ddccf1db7200870593fede3b8
sholiful-islam-emon <shorifulislamemon58@gmail.com>
2024-10-04 23:36:48 +0600

    second commit

--- lib/Screens/Home_screen.dart
+++ lib/Screens/Home_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_weather_bg_null_safety/bg/weather_bg.dart';
 import 'package:flutter_weather_bg_null_safety/utils/weather_type.dart';
 import 'package:get/get.dart';
-import 'package:get/get_common/get_reset.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:wather_app/Api_service/FutureDetails.dart';
 import 'package:wather_app/Api_service/State_service.dart';
 import 'package:wather_app/CustomWidget/Components/today_wather.dart';
@@ -38,9 +36,9 @@ class _HomeScreenState extends State<HomeScreen> {
         return WeatherType.lightSnow;
       } else if (current!.condition!.text!.contains("thunder")) {
         return WeatherType.thunder;
-      } else if (current!.condition!.text!.contains("showers")) {
+      } else if (current.condition!.text!.contains("showers")) {
         return WeatherType.middleSnow;
-      } else if (current!.condition!.text!.contains("rain")) {
+      } else if (current.condition!.text!.contains("rain")) {
         return WeatherType.heavyRainy;
       }
     } else {
@@ -58,9 +56,9 @@ class _HomeScreenState extends State<HomeScreen> {
         return WeatherType.lightSnow;
       } else if (current!.condition!.text!.contains("thunder")) {
         return WeatherType.thunder;
-      } else if (current!.condition!.text!.contains("showers")) {
+      } else if (current.condition!.text!.contains("showers")) {
         return WeatherType.middleSnow;
-      } else if (current!.condition!.text!.contains("rain")) {
+      } else if (current.condition!.text!.contains("rain")) {
         return WeatherType.heavyRainy;
       }
     }
@@ -108,151 +106,139 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
         backgroundColor: AppColors.White.withOpacity(.8),
-        body: GestureDetector(
-          onHorizontalDragDown: (details) {
-            setState(() {
-               CircularProgressIndicator();
-            });
-          },
-          child: Stack(
-            children: [
+        body: SafeArea(
+            child: FutureBuilder(
+          future: api_service.GetWatherApi(searchText),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              WeatherApiModel? watherapimodel = snapshot.data;
+              return Stack(
+                children: [
+                  WeatherBg(
+                    weatherType:GetWatherType(watherapimodel!.current),
+                    height: MediaQuery.sizeOf(context).height,
+                    width: MediaQuery.sizeOf(context).width,
+                  ),
 
-              SafeArea(
-                  child: FutureBuilder(
-                future: api_service.GetWatherApi(searchText),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    WeatherApiModel? watherapimodel = snapshot.data;
-                    return Stack(
+
+                  Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        WeatherBg(
-                          weatherType:GetWatherType(watherapimodel!.current),
-                          height: MediaQuery.sizeOf(context).height,
-                          width: MediaQuery.sizeOf(context).width,
-                        ),
-
-
-                        Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SizedBox(),
-                              Heddingone(
-                                  text: "Weather App",
-                                  TextColor: AppColors.primery_Color),
-                              Row(
-                                children: [
-                                  Container(
-                                      margin: const EdgeInsets.all(5),
-                                      decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Colors.white12),
-                                      child: IconButton(
-                                        onPressed: () async {
-                                          textfildcontroller.clear();
-                                          String text = await showAlartDialog(context);
-                                          setState(() {
-                                            searchText = text;
-                                          });
-                                        },
-                                        icon: const Icon(
-                                          Icons.search,
-                                          color: Colors.white,
-                                          size: 30,
-                                        ),
-                                      )),
-                                  Container(
-                                      margin: EdgeInsets.all(5),
-                                      decoration: BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Colors.white12),
-                                      child: IconButton(
-                                        onPressed: () {
-                                          setState(() {
-                                            searchText = "auto:ip" ;
-                                          });
-                                        },
-                                        icon: Icon(
-                                          Icons.location_city,
-                                          color: Colors.white,
-                                          size: 30,
-                                        ),
-                                      )),
-                                ],
-                              )
-                            ],
-                          ),
-                          TodayWather(watherApiModel: watherapimodel),
-                          Container(width: double.infinity,
-                            margin: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                                color: AppColors.White.withOpacity(.5),
-                                borderRadius: BorderRadius.circular(25.r)
-                            ),
-                            child: Heddingtwo(
-                                text: " Weather by hour ",
-                                TextColor: AppColors.secoderyColor),
-                          ),
-                          SizedBox(
-                              height: 164.h,
-                              child: ListView.builder(
-                                itemCount: watherapimodel!
-                                    .forecast!.forecastday![0].hour!.length,
-                                scrollDirection: Axis.horizontal,
-                                itemBuilder: (context, index) {
-                                  Hour hour = watherapimodel
-                                      .forecast!.forecastday![0].hour![index];
-                                  return Weatherbyhour(hour: hour);
-                                },
-                              )),
-                          Container(width: double.infinity,
-                            margin: EdgeInsets.all(10),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: AppColors.White.withOpacity(.5),
-                              borderRadius: BorderRadius.circular(25.r)
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Heddingtwo(
-                                  text: " Next 10 days details ",
-                                  TextColor: AppColors.secoderyColor),
-                            ),
-                          ),
-                          Expanded(
-                              child: ListView.builder(
-                            itemCount: watherapimodel.forecast!.forecastday!.length,
-                            itemBuilder: (context, index) {
-                              Forecastday forcastday =
-                                  watherapimodel.forecast!.forecastday![index];
-                              return Futuredetails(
-                                forecastday: forcastday,
-                              );
-                            },
-                          ))
-                        ],
-                      ),]
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(" Your city name is incorected press the back button",style: TextStyle( fontWeight: FontWeight.w700,fontSize: 25),textAlign: TextAlign.center,),
-                          ElevatedButton(onPressed: (){
-                            Get.to(HomeScreen());
-                          }, child: Text(" Back",style:TextStyle( color: AppColors.White,fontSize: 20),),style:ElevatedButton.styleFrom( backgroundColor: AppColors.primery_Color, ))
-                        ],
+                       const SizedBox(),
+                        Heddingone(
+                            text: "Weather App",
+                            TextColor: AppColors.White),
+                        Row(
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.all(5),
+                                decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white12),
+                                child: IconButton(
+                                  onPressed: () async {
+                                    textfildcontroller.clear();
+                                    String text = await showAlartDialog(context);
+                                    setState(() {
+                                      searchText = text;
+                                    });
+                                  },
+                                  icon: const Icon(
+                                    Icons.search,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
+                                )),
+                            Container(
+                                margin: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white12),
+                                child: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      searchText = "auto:ip" ;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    Icons.location_city,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
+                                )),
+                          ],
+                        )
+                      ],
+                    ),
+                    TodayWather(watherApiModel: watherapimodel),
+                    Container(width: double.infinity,
+                      margin: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: AppColors.White.withOpacity(.5),
+                          borderRadius: BorderRadius.circular(25.r)
                       ),
-                    );
-                  }
-                  return Center(child: CircularProgressIndicator(color: Colors.cyanAccent,strokeWidth: 5,));
-                },
-              )),
-            ],
-          ),
-        ));
+                      child: Heddingtwo(
+                          text: " Weather by hour ",
+                          TextColor: AppColors.secoderyColor),
+                    ),
+                    SizedBox(
+                        height: 164.h,
+                        child: ListView.builder(
+                          itemCount: watherapimodel
+                              .forecast!.forecastday![0].hour!.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            Hour hour = watherapimodel
+                                .forecast!.forecastday![0].hour![index];
+                            return Weatherbyhour(hour: hour);
+                          },
+                        )),
+                    Container(width: double.infinity,
+                      margin: EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: AppColors.White.withOpacity(.5),
+                        borderRadius: BorderRadius.circular(25.r)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Heddingtwo(
+                            text: " Next 10 days details ",
+                            TextColor: AppColors.secoderyColor),
+                      ),
+                    ),
+                    Expanded(
+                        child: ListView.builder(
+                      itemCount: watherapimodel.forecast!.forecastday!.length,
+                      itemBuilder: (context, index) {
+                        Forecastday forcastday =
+                            watherapimodel.forecast!.forecastday![index];
+                        return Futuredetails(
+                          forecastday: forcastday,
+                        );
+                      },
+                    ))
+                  ],
+                                        ),]
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(" Your city name is incorected press the back button",style: TextStyle( fontWeight: FontWeight.w700,fontSize: 25),textAlign: TextAlign.center,),
+                    ElevatedButton(onPressed: (){
+                      Get.to(HomeScreen());
+                    }, child: Text(" Back",style:TextStyle( color: AppColors.White,fontSize: 20),),style:ElevatedButton.styleFrom( backgroundColor: AppColors.primery_Color, ))
+                  ],
+                ),
+              );
+            }
+            return Center(child: CircularProgressIndicator(color: Colors.cyanAccent,strokeWidth: 5,));
+          },
+        )));
   }
 }
